9098bfa1bccc661ad37d26d5c6ed7ad171581909
Sokratis <sokratis.metallidis@agilox.net>
2023-11-01 09:04:22 +0100

    First commit

---
+++ apps/weather-backend/src/apis/geolocation.ts
@@ -0,0 +1,19 @@
+const request = require("request");
+
+export const geocode = (city, geoApiKey, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=${geoApiKey}`;
+  request({ url, json: true }, (err, res) => {
+    if (err) {
+      callback("Unable to connect!", undefined);
+    } else if (res.body.features.length === 0) {
+      callback("Location not found", undefined);
+    } else {
+      callback(undefined, {
+        latitude: res.body.features[0].center[1],
+        longitude: res.body.features[0].center[0],
+        location: res.body.features[0].place_name,
+      });
+    }
+  });
+};
+
--- apps/weather-backend/src/main.ts
+++ apps/weather-backend/src/main.ts
@@ -1,12 +1,34 @@
+import {geocode} from './apis/geolocation';
+import {forecast} from './apis/weather';
 import express from 'express';
+const weatherApiKey = process.env.WEATHER_API_KEY;
+const geoApiKey = process.env.GEO_API_KEY;
+
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.get("/weather", (req, res) => {
+  const { city = "Vienna" } = req.query;
+
+  geocode(city, geoApiKey, (err, geodata) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    forecast(geodata, weatherApiKey, (err, message) => {
+      if (err) {
+        res.send(err);
+        return;
+      }
+      res.send({
+        forecast: message,
+        location: geodata.location,
+        address: city,
+      });
+    });
+  });
 });
 
 app.listen(port, host, () => {
